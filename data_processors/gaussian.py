7cd630b1a8e6612b10d17315534f4731ba8d8226
shangdi <ubuntu@shangdiy-small.csail.mit.edu>
2023-07-23 19:54:01 -0400

    fix generator

--- data_processors/gaussian.py
+++ data_processors/gaussian.py
@@ -1,4 +1,6 @@
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 import sys
 
 assert(len(sys.argv) >= 3)
@@ -11,19 +13,24 @@ approx_n = int(sys.argv[2])
 
 cluster_sizes = []
 
+
 with open("./data/gaussian_%s_%s.data" % (cluster_n, approx_n), 'w') as f:
 	for i in range(cluster_n):
-		mx = np.random.uniform(0, 10)
-		my = np.random.uniform(0, 10)
+		mx = np.random.uniform(0, 100)
+		my = np.random.uniform(0, 100)
 		n = np.random.randint(approx_n // cluster_n // 1.5, approx_n // cluster_n * 2)
 		print(n)
 		cluster_sizes.append(n)
-		xs = np.random.normal(mx, 1, n)
-		ys = np.random.normal(my, 1, n)
+		xs = np.random.normal(mx, 2, n)
+		ys = np.random.normal(my, 2, n)
 		lines = [f'{str(x)} {str(y)}\n' for x, y in zip(xs, ys)]
 		f.writelines(lines)
+		sns.scatterplot(x = xs, y = ys)
+
 
 with open('./data/gaussian_%s_%s.gt'  % (cluster_n, approx_n), 'w') as f:
 	for i, cluster_size in enumerate(cluster_sizes):
 		f.writelines([f'{str(i+1)}\n' for j in range(cluster_size)])
 
+plt.savefig("./data/gaussian_%s_%s.png" % (cluster_n, approx_n))
+print("done")
