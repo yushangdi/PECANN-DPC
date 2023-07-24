808c93bc3c196f562527e9a153fdc60209fd7a79
shangdi <ubuntu@shangdiy-small.csail.mit.edu>
2023-07-23 22:37:18 -0400

    add options

--- doubling_dpc.cpp
+++ doubling_dpc.cpp
@@ -352,46 +352,38 @@ int main(int argc, char** argv){
 	std::string query_file, output_file, decision_graph_path;
 	float density_cutoff, dist_cutoff;
 	bool bruteforce = false;
+  unsigned int K = 6;
+  unsigned int L = 12;
+  unsigned int Lnn = 2;
+  unsigned int Lbuild = 12; 
+  unsigned int max_degree = 16;
+  float alpha = 1.2;
 
-	// TODO: change to use parse_command_line.h
-	po::options_description desc{"Arguments"};
- 	try {
-	    desc.add_options()("query_file",
-                       po::value<std::string>(&query_file)->required(),
-                       "Query file");
-	    desc.add_options()("output_file",
-                       po::value<std::string>(&output_file)->default_value(""),
-                       "Output cluster file");
-			desc.add_options()("decision_graph_path",
-                       po::value<std::string>(&decision_graph_path)->default_value(""),
-                       "Output decision_graph_path");	    
-			desc.add_options()("density_cutoff",
-				       po::value<float>(&density_cutoff)->default_value(0),
-				       "Density below which points are treated as noise");
-	    desc.add_options()("dist_cutoff",
-				       po::value<float>(&dist_cutoff)->default_value(std::numeric_limits<float>::max()),
-				       "Distance below which points are sorted into the same cluster");
-			desc.add_options()("bruteforce",
-				       po::value<bool>(&bruteforce)->default_value(false),
-				       "Whether bruteforce method is used.");
-			
-	    po::variables_map vm;
-	    po::store(po::parse_command_line(argc, argv, desc), vm);
-    	if (vm.count("help")) {
-	    	std::cout << desc;
-    		return 0;
-	    }
-    	po::notify(vm);	
-	}catch(const std::exception& ex) {
-    	std::cerr << ex.what() << '\n';
-	    return -1;
-	}
-  const unsigned K = 6;
-  const unsigned L = 12;
-  const unsigned Lnn = 2;
-  const unsigned Lbuild = 12; 
-  const unsigned max_degree = 16;
-  const float alpha = 1.2;
+	po::options_description desc("Allowed options");
+    desc.add_options()
+        ("K", po::value<unsigned int>(&K)->default_value(6), "the number of nearest neighbor used for computing the density.")
+        ("L", po::value<unsigned int>(&L)->default_value(12), "L value used for density computation.")
+        ("Lnn", po::value<unsigned int>(&Lnn)->default_value(2), "Lnn value used for dependent point computation.")
+        ("Lbuild", po::value<unsigned int>(&Lbuild)->default_value(12), "Retain closest Lbuild number of nodes during the greedy search of construction.")
+        ("max_degree", po::value<unsigned int>(&max_degree)->default_value(16), "max_degree value used for constructing the graph.")
+        ("alpha", po::value<float>(&alpha)->default_value(1.2), "alpha value")
+        ("query_file", po::value<std::string>(&query_file)->required(), "Data set file")
+        ("output_file", po::value<std::string>(&output_file)->default_value(""), "Output cluster file")
+        ("decision_graph_path", po::value<std::string>(&decision_graph_path)->default_value(""), "Output decision_graph_path")
+        ("density_cutoff", po::value<float>(&density_cutoff)->default_value(0), "Density below which points are treated as noise")
+        ("dist_cutoff", po::value<float>(&dist_cutoff)->default_value(std::numeric_limits<float>::max()), "Distance below which points are sorted into the same cluster")
+        ("bruteforce", po::value<bool>(&bruteforce)->default_value(false), "Whether bruteforce method is used.")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return 1;
+    }
+
 
 	Method method = Method::Doubling;
 
@@ -403,7 +395,5 @@ int main(int argc, char** argv){
 		dpc(K, L, Lnn, query_file, density_cutoff, dist_cutoff, 
 				output_file, decision_graph_path, Lbuild, max_degree, alpha, method);
 	}
-
-
 
 }
