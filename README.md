09a54636b93a479588de40870e28bf6c50a470e5
shangdi <ubuntu@shangdiy-small.csail.mit.edu>
2023-07-23 20:09:28 -0400

    plot decision graph

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # DPC-ANN
 
 
+Change line 3 in Makefile to the location of boost library.
+
+
+```bash
+make
+mkdir results
+./doubling_dpc --query_file ./data/gaussian_4_1000.data --decision_graph_path ./results/gaussian_4_1000.dg 
 ```
-sudo apt install libboost-all-dev
+
+plot decision graph:
+
+```bash
+python3 post_processors/plot_decision_graph.py results/gaussian_4_1000.dg 
 ```
--- post_processors/plot_decision_graph.py
+++ post_processors/plot_decision_graph.py
@@ -7,6 +7,11 @@ assert(len(sys.argv) >= 2)
 
 decision_graph_path = sys.argv[1]
 
+num_cluster = -1
+
+if (len(sys.argv) > 2):
+	num_cluster = int(sys.argv[2])
+
 print(f'reading decision_graph from {decision_graph_path}')
 
 with open(decision_graph_path, 'r') as file:
@@ -22,7 +27,30 @@ for i in range(len(data)):
 		data[i, 1] = max_dist
 
 sns.scatterplot(x=data[:, 0], y=data[:, 1])
+
+
+if i != -1:
+	# Extract the second column
+	second_col = data[:, 1]
+
+	# Sort the second column in descending order
+	sorted_vals = np.sort(second_col)[::-1]
+
+	# Check if i+1 exists in the list
+	if num_cluster < len(sorted_vals) - 1:
+			# Get the number between ith and i+1th largest values
+			mid_value = (sorted_vals[num_cluster-1] + sorted_vals[num_cluster]) / 2
+			print("distance threshold", mid_value)
+			plt.axhline(y = mid_value)
+			y_ticks = plt.gca().get_yticks().tolist()
+			y_ticks.append(mid_value)
+			plt.yticks(sorted(y_ticks))
+	else:
+			print("num_cluster+1th largest value doesn't exist.", num_cluster)
+
+
+
 plt.xlabel("density")
 plt.ylabel("distance to denpendent point")
-plt.savefig('decision_graph.png')
+plt.savefig('results/decision_graph.png')
 print("done")
