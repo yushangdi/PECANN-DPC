96bb5733197dc2500f2b4481ea0b41d0c1c1440f
Josh Engels <jengels@mit.edu>
2023-09-12 21:52:41 -0400

    Fixing defaults

--- src/python_bindings.cpp
+++ src/python_bindings.cpp
@@ -9,10 +9,11 @@ using namespace nb::literals;
 
 NB_MODULE(dpc_ann_ext, m) {
   m.def("dpc", &DPC::dpc_temp, "data_path"_a, "K"_a = 6, "L"_a = 12,
-        "Lnn"_a = 4, "density_cutoff"_a = 0, "distance_cutoff"_a = 0,
-        "center_density_cutoff"_a = std::numeric_limits<float>::max(),
-        "output_path"_a = "", "decision_graph_path"_a = "", "Lbuild"_a = 12,
-        "max_degree"_a = 16, "alpha"_a = 1.2, "num_clusters"_a = 4,
-        "method"_a = "Doubling", "graph_type"_a = "Vamana",
+        "Lnn"_a = 4, "density_cutoff"_a = 0,
+        "distance_cutoff"_a = std::numeric_limits<float>::max(),
+        "center_density_cutoff"_a = 0, "output_path"_a = "",
+        "decision_graph_path"_a = "", "Lbuild"_a = 12, "max_degree"_a = 16,
+        "alpha"_a = 1.2, "num_clusters"_a = 4, "method"_a = "Doubling",
+        "graph_type"_a = "Vamana",
         "This function clusters the passed in files.");
 }
