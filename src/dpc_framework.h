ee60bcff5478666a4c1e139d4c1dfe7e6b5fe49f
shangdi <ubuntu@shangdiy-small.csail.mit.edu>
2023-09-20 11:50:40 -0400

    implement our algorithm in framework

--- src/dpc_framework.h
+++ src/dpc_framework.h
@@ -8,6 +8,7 @@
 #include <iomanip>
 #include <iostream>
 #include <numeric>
+#include <optional>
 #include <set>
 #include <sstream>
 #include <utility>
@@ -45,6 +46,8 @@ struct DatasetKnn {
   size_t aligned_dim_;
   const float *data_;
   const Distance *D_;
+  // the i*k+j's element is i's jth nearest neighbor.
+  // the distance is squared distance
   const std::pair<int, double> *knn_;
   const int k_;
 
@@ -53,69 +56,22 @@ struct DatasetKnn {
       : num_data_(raw_data.num_data), data_dim_(raw_data.data_dim),
         aligned_dim_(raw_data.aligned_dim), data_(raw_data.data), D_(D),
         knn_(knn.data()), k_(k) {}
+
+  // return the dependent point of i if it's in i'th knn, return nullopt
+  // otherwise.
+  std::optional<std::pair<int, float>>
+  get_dep_ptr(int i, std::vector<double> &densities) {
+    double d_i = densities[i];
+    for (size_t j = 0; j < k_; ++j) {
+      int id = knn_[i * k_ + j].first;
+      float d_j = densities[id];
+      if (d_j > d_i || (d_i == d_i && id > i)) {
+        return knn_[i * k_ + j].second;
+      }
+    }
+    return std::nullopt;
+  }
 };
-
-// Construct graph for `data`. Parameters:
-//   raw_data: raw input data points corresponding to data.
-//   D: distance computation method
-//   L: the buffer size parameter
-template <class T>
-parlay::sequence<Tvec_point<T> *>
-construct_graph(const RawDataset &raw_data, const unsigned L, const float alpha,
-                const int max_degree, const Distance *D,
-                const GraphType graph_type);
-
-// Compute the k-nearest neighbors of points in `graph` using graph-based
-// method. Parameters:
-//   graph: input data points. They should have neighbors computed already.
-//   raw_data: raw input data points corresponding to data.
-//   D: distance computation method
-//   K: the k in knn
-//   L: the buffer size parameter
-template <class T>
-std::vector<std::pair<int, double>>
-compute_knn(parlay::sequence<Tvec_point<T> *> &graph,
-            const RawDataset &raw_data, const unsigned K, const unsigned L,
-            const Distance *D);
-
-// Compute the k-nearest neighbors of points in `raw_data` using bruteforce
-// method. Parameters:
-//   raw_data: raw input data points corresponding to data.
-//   D: distance computation method
-//   K: the k in knn
-template <class T>
-std::vector<std::pair<int, double>>
-compute_knn_bruteforce(const RawDataset &raw_data, const unsigned K,
-                       const Distance *D);
-
-// Compute the dependet point of points in `graph` with densities above
-// `density_cutoff` using graph-based method. Parameters:
-//   graph: input data points. They should have neighbors computed already.
-//   raw_data: raw input data points corresponding to data.
-//   densities: densities of data pints.
-//   density_cutoff: points with densities below this threshold are considered
-//   noise points, and their dependent points are not computed.
-//   D: distance computation method
-//   L: the buffer size parameter
-//   round_limit: the round limit parameter
-template <class T>
-std::vector<std::pair<int, double>>
-compute_dep_ptr(parlay::sequence<Tvec_point<T> *> &graph,
-                const RawDataset &raw_data, const std::vector<T> &densities,
-                const Distance *D, unsigned L, int round_limit = -1);
-
-// Compute the dependet point of `raw_data` with densities above
-// `density_cutoff` using bruteforce method. Parameters:
-//   raw_data: input data points.
-//   densities: densities of data pints.
-//   density_cutoff: points with densities below this threshold are considered
-//   noise points, and their dependent points are not computed.
-//   D: distance computation method
-template <class T>
-std::vector<std::pair<int, double>>
-compute_dep_ptr_bruteforce(const RawDataset &raw_data,
-                           const std::vector<T> &densities,
-                           const float density_cutoff, Distance *D);
 
 // Base class for other DPCComputers.
 class DPCComputer {
@@ -132,6 +88,18 @@ protected:
       : knn_(data_knn.knn_), D_(data_knn.D_), num_data_(data_knn.num_data_),
         data_dim_(data_knn.data_dim_), aligned_dim_(data_knn.aligned_dim_),
         data_(data_knn.data_), k_(data_knn.k_) {}
+
+  DPCComputer() {}
+
+  void initialize(const DatasetKnn &data_knn) {
+    knn_ = data_knn.knn_;
+    D_ = data_knn.D_;
+    num_data_ = data_knn.num_data_;
+    data_dim_ = data_knn.data_dim_;
+    aligned_dim_ = data_knn.aligned_dim_;
+    data_ = data_knn.data_;
+    k_ = data_knn.k_;
+  }
 
   virtual ~DPCComputer() {} // Virtual destructor
 };
@@ -151,7 +119,7 @@ public:
 
 template <typename T> class CenterFinder : public DPCComputer {
 public:
-  CenterFinder(const DatasetKnn &data_knn) : DPCComputer(data_knn) {}
+  CenterFinder() : DPCComputer() {}
 
   virtual ~CenterFinder() {}
 
@@ -164,7 +132,7 @@ public:
 
 template <typename T> class NoiseFinder : public DPCComputer {
 public:
-  NoiseFinder(const DatasetKnn &data_knn) : DPCComputer(data_knn) {}
+  NoiseFinder() : DPCComputer() {}
 
   virtual ~NoiseFinder() {}
 
@@ -172,41 +140,31 @@ public:
                                    std::vector<T> &re_weighted_densities) = 0;
 };
 
-template <typename T> class ThresholdCenterFinder : public CenterFinder<T> {
-private:
-  double delta_threshold_;
-  double density_threshold_;
-
-public:
-  ThresholdCenterFinder(const DatasetKnn &data_knn, double delta_threshold,
-                        double density_threshold)
-      : DPCComputer(data_knn), delta_threshold_(delta_threshold),
-        density_threshold_(density_threshold) {}
-
-  virtual ~ThresholdCenterFinder() {}
-
-  virtual std::set<int>
-  operator()(std::vector<T> &densities, std::vector<T> &re_weighted_densities,
-             std::vector<std::pair<uint32_t, double>> &dep_ptrs) = 0;
-};
-
 template <typename T> class ClusterAssigner : public DPCComputer {
 public:
-  ClusterAssigner(const DatasetKnn &data_knn) : DPCComputer(data_knn) {}
+  ClusterAssigner() : DPCComputer() {}
 
   virtual std::vector<int>
   operator()(std::vector<T> &densities, std::vector<T> &re_weighted_densities,
-             std::vector<std::pair<uint32_t, double>> &dep_ptrs) = 0;
+             std::vector<std::pair<uint32_t, double>> &dep_ptrs,
+             std::set<int> &centers) = 0;
 };
 
 template <typename T> class ClusterMerger : public DPCComputer {
 public:
-  ClusterMerger(const DatasetKnn &data_knn) : DPCComputer(data_knn) {}
+  ClusterMerger() : DPCComputer() {}
 
   virtual std::vector<int>
   operator()(std::vector<int> &current_cluster, std::vector<T> &densities,
              std::vector<T> &re_weighted_densities,
              std::vector<std::pair<uint32_t, double>> &dep_ptrs) = 0;
 };
+
+void dpc(const unsigned K, const unsigned L, const unsigned Lnn,
+         RawDataset raw_data, float density_cutoff, float distance_cutoff,
+         float center_density_cutoff, const std::string &output_path,
+         const std::string &decision_graph_path, const unsigned Lbuild,
+         const unsigned max_degree, const float alpha,
+         const unsigned num_clusters, Method method, GraphType graph_type);
 
 } // namespace DPC
