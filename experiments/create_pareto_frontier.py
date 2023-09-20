41e7fa4777f690bba1925ae4768b021bd8f1b86d
Joshua Engels <josh.adam.engels@gmail.com>
2023-09-20 15:29:38 -0400

    Experiment fixes / updates (#11)
    * More experiment fixes
    
    * More fixes
    
    * Fixing graph name
    
    * Always recreate ground truth clustering
    
    * Forgot to move this out too
    
    * Expanding range
    
    * Adding extra arg to product
    
    * Updating parlay
    
    ---------
    
    Co-authored-by: Josh Engels <jengels@mit.edu>

--- experiments/create_pareto_frontier.py
+++ experiments/create_pareto_frontier.py
@@ -20,7 +20,7 @@ from utils import (
     create_results_file,
     eval_cluster_and_write_results,
     make_results_folder,
-    get_cutoff,
+    product_cluster_dg,
 )
 
 
@@ -30,7 +30,12 @@ parser.add_argument(
     help="Dataset name (should be a file named data/<dataset>/<dataset>.npy and data/dataset/<dataset>gt).",
 )
 parser.add_argument(
-    "timeout",
+    "num_clusters",
+    help="How many clusters to use when generating the clusters from the decision graph files",
+    type=int,
+)
+parser.add_argument(
+    "--timeout",
     help="How long to wait in seconds before killing one of the running jobs",
     default=20,
     type=int,
@@ -39,6 +44,7 @@ args = parser.parse_args()
 
 dataset = args.dataset
 timeout_s = args.timeout
+num_clusters = args.num_clusters
 
 
 cluster_results_file = create_results_file()
@@ -50,62 +56,78 @@ options = []
 # can add others once we know reasonable parameters for num_clusters
 #  and reduce the search space with the vamana search
 
-# For now not including 128 at top of range
-
-exponential_range = [4, 8, 16, 32, 64]
+exponential_range = [8, 16, 32, 64, 128, 256]
 for (
     max_degree,
     beam_search_construction,
     beam_search_clustering,
-) in itertools.product(exponential_range, exponential_range, exponential_range):
+    beam_search_density
+) in itertools.product(exponential_range, exponential_range, exponential_range, exponential_range):
     # We are assuming Vamana value of alpha = 1.1 (experimentally verified) works well for other graph methods
     # TODO(Josh): Validate this assumption? Can just leave running in background somewhere
     # for alpha in [1, 1.05, 1.1, 1.15, 1.2]:
     for alpha in [1.1]:
-        for beam_search_density in [8, 16, 32, 64]:
-            for graph_type in ["Vamana", "pyNNDescent", "HCNNG"]:
-                method = f"{graph_type}_{max_degree}_{alpha}_{beam_search_construction}_{beam_search_density}_{beam_search_clustering}"
-                command_line = {
-                    "max_degree": max_degree,
-                    "alpha": alpha,
-                    "Lbuild": beam_search_construction,
-                    "L": beam_search_density,
-                    "Lnn": beam_search_clustering,
-                    "graph_type": graph_type,
-                }
-                if graph_type in ["pyNNDescent", "HCNNG"]:
-                    if beam_search_construction < 16:
-                        continue
-                    for num_clusters in range(1, 6):
-                        new_command_line = dict(command_line)
-                        command_line["num_clusters"] = num_clusters
-                        options.append((method, new_command_line))
-                else:
-                    options.append((method, command_line))
+        # for graph_type in ["Vamana", "pyNNDescent", "HCNNG"]:
+        for graph_type in ["Vamana"]:
+            method = f"{graph_type}_{max_degree}_{alpha}_{beam_search_construction}_{beam_search_density}_{beam_search_clustering}"
+            command_line = {
+                "max_degree": max_degree,
+                "alpha": alpha,
+                "Lbuild": beam_search_construction,
+                "L": beam_search_density,
+                "Lnn": beam_search_clustering,
+                "graph_type": graph_type,
+            }
+            if graph_type in ["pyNNDescent", "HCNNG"]:
+                if beam_search_construction < 16:
+                    continue
+                for num_clusters in range(1, 6):
+                    new_command_line = dict(command_line)
+                    command_line["num_clusters"] = num_clusters
+                    options.append((method, new_command_line))
+            else:
+                options.append((method, command_line))
 
 dataset_folder = make_results_folder(dataset)
 
 data = np.load(f"data/{dataset_folder}/{dataset}.npy").astype("float32")
 
+
+def create_product_clustering(decision_graph_path, num_clusters, output_path):
+    clusters = product_cluster_dg(decision_graph_path, num_clusters)
+    clusters = clusters.reshape((len(clusters), 1))
+    np.savetxt(output_path, clusters, fmt="%i")
+
+
 ground_truth_cluster_path = f"results/{dataset_folder}/{dataset}_BruteForce.cluster"
+ground_truth_decision_graph_path = f"results/{dataset_folder}/{dataset}_BruteForce.dg"
 if not os.path.isfile(ground_truth_cluster_path):
     dpc_ann.dpc_numpy(
         graph_type="BruteForce",
-        decision_graph_path=f"results/{dataset_folder}/{dataset}_BruteForce.dg",
-        output_path=ground_truth_cluster_path,
-        **get_cutoff(dataset),
+        decision_graph_path=ground_truth_decision_graph_path,
+        # output_path=ground_truth_cluster_path,
+        # **get_cutoff(dataset),
         data=data,
     )
+# Always recreate ground truth clustering
+create_product_clustering(
+    ground_truth_decision_graph_path, num_clusters, ground_truth_cluster_path
+)
 
 
 def try_command(graph_type, command):
-    prefix = f"results/{dataset_folder}/{dataset}_{method}"
+    prefix = f"results/{dataset_folder}/{dataset}_{graph_type}"
 
     times = dpc_ann.dpc_numpy(
         **command,
-        **get_cutoff(dataset),
+        # **get_cutoff(dataset),
         data=data,
         decision_graph_path=f"{prefix}.dg",
+        # output_path=f"{prefix}.cluster",
+    )
+    create_product_clustering(
+        decision_graph_path=f"{prefix}.dg",
+        num_clusters=num_clusters,
         output_path=f"{prefix}.cluster",
     )
 
--- post_processors/cluster_eval.py
+++ post_processors/cluster_eval.py
@@ -7,65 +7,89 @@ import os
 import json
 import sys
 
-def eval_clusters(labels, preds, verbose=True, eval_metrics=["recall", "ami", "ari", "completeness", "homogeneity"]):
 
-	label_counter = Counter(labels)
-	pred_counter = Counter(preds)
-	if verbose:
-		print('groud truth', label_counter)
-		print('clustering', pred_counter)
-	
-	result = {}
+def eval_clusters(
+    labels,
+    preds,
+    verbose=True,
+    eval_metrics=[
+        "recall50",
+        "precision50",
+        "AMI",
+        "ARI",
+        "completeness",
+        "homogeneity",
+    ],
+):
+    label_counter = Counter(labels)
+    pred_counter = Counter(preds)
+    if verbose:
+        print("groud truth", label_counter)
+        print("clustering", pred_counter)
 
-	if "recall" in eval_metrics:
-		TP_count = 0
-		for label, label_count in label_counter.items():
-			ids = np.argwhere(labels == label)[:,0]
-			pred, pred_count = stats.mode(preds[ids], axis=None, keepdims=False)
-			# print(label, pred)
-			if pred_count / (label_count + pred_counter[pred] - pred_count) > 0.5 :
-				# print('pass', label, pred)
-				TP_count += 1
+    result = {}
 
-		recall50 = TP_count / len(label_counter)
-		precision50 = TP_count / len(pred_counter)
+    if "recall50" in eval_metrics or "precision50" in eval_metrics:
+        TP_count = 0
+        for label, label_count in label_counter.items():
+            ids = np.argwhere(labels == label)[:, 0]
+            pred, pred_count = stats.mode(preds[ids], axis=None, keepdims=False)
+            # print(label, pred)
+            if pred_count / (label_count + pred_counter[pred] - pred_count) > 0.5:
+                # print('pass', label, pred)
+                TP_count += 1
 
-		result['recall50'] = recall50
-		result['precision50'] = precision50
+        recall50 = TP_count / len(label_counter)
+        precision50 = TP_count / len(pred_counter)
+
+        if "recall50" in eval_metrics:
+            result["recall50"] = recall50
+        if "precision50" in eval_metrics:
+            result["precision50"] = precision50
+
+    if "AMI" in eval_metrics:
+        result["AMI"] = sklearn.metrics.adjusted_mutual_info_score(labels, preds)
+
+    if "ARI" in eval_metrics:
+        result["ARI"] = sklearn.metrics.adjusted_rand_score(labels, preds)
+
+    if "completeness" in eval_metrics:
+        result["completeness"] = sklearn.metrics.completeness_score(labels, preds)
+
+    if "homogeneity" in eval_metrics:
+        result["homogeneity"] = sklearn.metrics.homogeneity_score(labels, preds)
+
+    return result
 
 
-	if "ami" in eval_metrics:
-		result["AMI"] = sklearn.metrics.adjusted_mutual_info_score(labels, preds)
+def eval_cluster_files(
+    gt_path,
+    cluster_path,
+    verbose=True,
+    eval_metrics=[
+        "recall50",
+        "precision50",
+        "AMI",
+        "ARI",
+        "completeness",
+        "homogeneity",
+    ],
+):
+    if verbose:
+        print(f"reading gt from {gt_path}")
+        print(f"reading result from {cluster_path}")
 
-	if "ari" in eval_metrics:
-		result["ARI"] = sklearn.metrics.adjusted_rand_score(labels, preds)
+    with open(gt_path, "r") as file:
+        labels = np.array([int(line.rstrip()) for line in file])
 
-	if "completeness" in eval_metrics:
-		result['completeness'] = sklearn.metrics.completeness_score(labels, preds)
+    with open(cluster_path, "r") as file:
+        preds = np.array([int(line.rstrip()) for line in file])
 
-	if "homogeneity" in eval_metrics:
-		result['homogeneity'] = sklearn.metrics.homogeneity_score(labels, preds)
+    return eval_clusters(labels, preds, verbose, eval_metrics)
 
-	return result
-
-
-def eval_cluster_files(gt_path, cluster_path, verbose=True):
-
-	if verbose:
-		print(f'reading gt from {gt_path}')
-		print(f'reading result from {cluster_path}')
-
-	with open(gt_path, 'r') as file:
-		labels = np.array([int(line.rstrip()) for line in file])
-
-	with open(cluster_path, 'r') as file:
-		preds = np.array([int(line.rstrip()) for line in file])
-
-	return eval_clusters(labels, preds, verbose)
 
 if __name__ == "__main__":
-	assert(len(sys.argv) >= 3)
-	gt_path = sys.argv[1]
-	cluster_path = sys.argv[2]
-	print(json.dumps(eval_cluster_files(gt_path, cluster_path), indent=4))
-
+    assert len(sys.argv) >= 3
+    gt_path = sys.argv[1]
+    cluster_path = sys.argv[2]
+    print(json.dumps(eval_cluster_files(gt_path, cluster_path), indent=4))
