e435963c64d282754efec4e0bb4b4bc49e0387aa
Josh Engels <jengels@mit.edu>
2023-09-27 11:49:07 -0400

    Adding nredo

--- experiments/test_kmeans.py
+++ experiments/test_kmeans.py
@@ -19,42 +19,45 @@ ncentroids = 1000
 
 results_file = create_results_file("kmeans")
 
+for nredo in range(1, 5):
+    for niter in list(range(1, 10)) + list(range(10, 50, 5)):
+        if nredo * niter > 100:
+            continue
+        
+        built_start_time = time.time()
+        verbose = False
+        d = x.shape[1]
+        kmeans = faiss.Kmeans(d, ncentroids, niter=niter, verbose=verbose)
+        kmeans.train(x)
+        built_time = time.time() - built_start_time
 
-for niter in list(range(1, 10)) + list(range(10, 50, 5)):
-    built_start_time = time.time()
-    verbose = False
-    d = x.shape[1]
-    kmeans = faiss.Kmeans(d, ncentroids, niter=niter, verbose=verbose)
-    kmeans.train(x)
-    built_time = time.time() - built_start_time
+        find_clusters_start = time.time()
+        batch_size = 100000
+        clusters = []
+        for start in range(0, len(x), batch_size):
+            batch_x = x[start : start + batch_size]
+            distances = torch.cdist(torch.tensor(batch_x), torch.tensor(kmeans.centroids))
+            clusters += torch.argmin(distances, dim=1).tolist()
+        find_clusters_time = time.time() - find_clusters_start
 
-    find_clusters_start = time.time()
-    batch_size = 100000
-    clusters = []
-    for start in range(0, len(x), batch_size):
-        batch_x = x[start : start + batch_size]
-        distances = torch.cdist(torch.tensor(batch_x), torch.tensor(kmeans.centroids))
-        clusters += torch.argmin(distances, dim=1).tolist()
-    find_clusters_time = time.time() - find_clusters_start
+        total_time = time.time() - built_start_time
+        with open("results/kmeans.cluster", "w") as f:
+            for i in clusters:
+                f.write(str(i) + "\n")
 
-    total_time = time.time() - built_start_time
-    with open("results/kmeans.cluster", "w") as f:
-        for i in clusters:
-            f.write(str(i) + "\n")
+        method_name = f"kmeans_{niter}_{nredo}"
+        times = {
+            "Total": total_time,
+            "Find clusters": find_clusters_time,
+            "Built index": built_time,
+        }
 
-    method_name = f"kmeans_{niter}"
-    times = {
-        "Total": total_time,
-        "Find clusters": find_clusters_time,
-        "Built index": built_time,
-    }
-
-    eval_cluster_and_write_results(
-        gt_cluster_path=f"data/imagenet/imagenet.gt",
-        cluster_path=f"results/kmeans.cluster",
-        compare_to_ground_truth=True,
-        results_file=results_file,
-        dataset="imagenet",
-        method=method_name,
-        time_reports=times,
-    )
+        eval_cluster_and_write_results(
+            gt_cluster_path=f"data/imagenet/imagenet.gt",
+            cluster_path=f"results/kmeans.cluster",
+            compare_to_ground_truth=True,
+            results_file=results_file,
+            dataset="imagenet",
+            method=method_name,
+            time_reports=times,
+        )
