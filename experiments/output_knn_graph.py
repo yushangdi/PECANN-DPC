6d89506382584af0866049c3d629b725c2fac48a
shangdi <yushangdi123@gmail.com>
2023-11-07 16:32:19 -0500

    exp

--- experiments/output_knn_graph.py
+++ experiments/output_knn_graph.py
@@ -18,15 +18,21 @@ os.chdir(abspath)
 sys.path.append(str(abspath))
 
 # data = np.load(f"data/{dataset_folder}/{dataset}.npy").astype("float32")
-digits = datasets.load_digits()
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1)).astype("float32")
-knn_graph_path = "results/knn_graphs/digits.graph.txt"
-dpc_ann.dpc_numpy(
-            graph_type="BruteForce",
-            knn_graph_path=knn_graph_path,
-            data=data,
-            K=10,
-            center_finder=dpc_ann.ProductCenterFinder(num_clusters=1),
-        )
+# digits = datasets.load_digits()
+# n_samples = len(digits.images)
+# data = digits.images.reshape((n_samples, -1)).astype("float32")
+# knn_graph_path = "results/knn_graphs/digits.graph.txt"
+
+data = np.load("data/mnist/mnist.npy")
+print("data loaded")
+ks = [10, 50]
+for k in ks:
+  knn_graph_path = "results/knn_graphs/mnist_k%s.graph.txt" % k
+  dpc_ann.dpc_numpy(
+              graph_type="BruteForce",
+              knn_graph_path=knn_graph_path,
+              data=data,
+              K=k,
+              center_finder=dpc_ann.ProductCenterFinder(num_clusters=1),
+          )
 
