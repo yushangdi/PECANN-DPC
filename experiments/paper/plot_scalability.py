19b370978f967cb27ee69958ba71f108672bda3e
Josh Engels <josh.adam.engels@gmail.com>
2023-11-10 11:59:12 -0500

    Fixing scalability experiment

---
+++ experiments/paper/plot_scalability.py
@@ -0,0 +1,27 @@
+import argparse
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_scalability_by_dataset_size(csv_file):
+    df = pd.read_csv(csv_file)
+
+    grouped_data = df.groupby('num_threads')
+
+    for name, group in grouped_data:
+        print(group.columns.values.tolist())
+        plt.plot(group['dataset'].str[9:].astype('int64'), group['Total time'], label=f"{name} threads")
+
+    plt.xlabel('Dataset Size')
+    plt.ylabel('Total Time')
+    plt.legend(title='Dataset')
+    plt.savefig("results/paper/synthetic.png")
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Plot total time vs dataset size for each number of threads')
+
+    parser.add_argument('csv_file', type=str, help='Path to the CSV file containing the scalability experiment data data')
+
+    args = parser.parse_args()
+
+    plot_scalability_by_dataset_size(args.csv_file)
--- experiments/paper/scalability.py
+++ experiments/paper/scalability.py
@@ -1,9 +1,9 @@
-import dpc_ann
 import os
 from pathlib import Path
 import sys
 import numpy as np
 from paper_utils import get_core_groups, generate_synthetic_data
+import multiprocessing
 
 abspath = Path(__file__).resolve().parent.parent
 sys.path.append(str(abspath))
@@ -16,31 +16,16 @@ from utils import (
     make_results_folder,
 )
 
-initial_threads = os.sched_getaffinity(0)
-
-cluster_results_file = create_results_file(prefix="scalability")
-dataset_folder = make_results_folder("synthetic")
-
 num_datapoints_to_cluster = [10**5, 10**6, 10**7, 10**8, 10**9]
 num_clusters = 100
-
-current_threads = []
-core_groups = get_core_groups()
-
-
-def flatten(l):
-    return [item for sublist in l for item in sublist]
+initial_threads = os.sched_getaffinity(0)
+dataset_folder = make_results_folder("synthetic")
 
 
-threads = flatten(core_groups)
-
-print(threads)
-for thread in threads:
-    current_threads.append(thread)
-    if len(current_threads) not in [1, 2, 4, 8, 16, 32, 64, 128, 256]:
-        continue
+def run_synthetic(current_threads, cluster_results_file):
+    import dpc_ann
     os.environ["PARLAY_NUM_THREADS"] = f"{len(current_threads)}"
-    print(f"Running experiment with {len(current_threads)} threads: {current_threads}")
+    print(f"Running experiment with {len(current_threads)} threads: {current_threads}", flush=True)
 
     # TODO: Do we want to do something to ensure accuracy is above a certain threshold?
     for num_datapoints in num_datapoints_to_cluster:
@@ -75,7 +60,34 @@ for thread in threads:
             found_clusters=np.array(clustering_result.clusters),
             comparing_to_ground_truth=True,
             results_file=cluster_results_file,
-            dataset="synthetic",
+            dataset=f"synthetic{num_datapoints}",
             method="Vamana_standard",
             time_reports=clustering_result.metadata,
         )
+
+
+def run_experiment():
+
+    cluster_results_file = create_results_file(prefix="scalability")
+
+    current_threads = []
+    core_groups = get_core_groups()
+
+
+    def flatten(l):
+        return [item for sublist in l for item in sublist]
+
+
+    threads = flatten(core_groups)
+
+    print(threads)
+    for thread in threads:
+        current_threads.append(thread)
+        if len(current_threads) in [1, 2, 4, 8, 16, 32, 64, 128, 256]:
+            p = multiprocessing.Process(target=run_synthetic, args=(current_threads, cluster_results_file))
+            p.start()
+            p.join()
+    
+
+if __name__ == "__main__":
+    run_experiment()
