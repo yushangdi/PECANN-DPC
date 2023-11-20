d8d9acc86a5afd96a50baff11c0938fc45c4b7fd
Josh Engels <jengels@mit.edu>
2023-11-19 22:12:55 -0500

    Undoing extra formatting changes

--- tests/knn_ground_truth.py
+++ tests/knn_ground_truth.py
@@ -1,36 +1,32 @@
 import numpy as np
 
-
 def compute_knn_and_distances(data, k):
     num_points = len(data)
     distances = np.zeros((num_points, num_points))
-
+    
     # Compute pairwise squared distances
     for i in range(num_points):
         for j in range(num_points):
             distances[i, j] = np.sum((data[i] - data[j]) ** 2)
-
+    
     knn_indices = np.argsort(distances, axis=1)[:, 0:k]  # Exclude the point itself
-    knn_distances = np.sort(distances, axis=1)[:, 0:k]  # Exclude the point itself
-
+    knn_distances = np.sort(distances, axis=1)[:, 0:k]   # Exclude the point itself
+    
     return knn_indices, knn_distances
 
-
 # Sample data
-data = np.array(
-    [
-        [1, 2],
-        [2, 4],
-        [3, 6],
-        [4, 8],
-        [5, 10],
-        [6, 12],
-        [7, 14],
-        [8, 16],
-        [9, 18],
-        [10, 20],
-    ]
-)
+data = np.array([
+    [1, 2],
+    [2, 4],
+    [3, 6],
+    [4, 8],
+    [5, 10],
+    [6, 12],
+    [7, 14],
+    [8, 16],
+    [9, 18],
+    [10, 20]
+])
 
 k = 3
 knn_indices, knn_distances = compute_knn_and_distances(data, k)
--- tests/test_framework_commandline.py
+++ tests/test_framework_commandline.py
@@ -1,49 +1,31 @@
 import subprocess
 import sys
-
-sys.path.append("/home/ubuntu/DPC-ANN")
+sys.path.append('/home/ubuntu/DPC-ANN')
 from post_processors import cluster_eval
 
 ## test dpc_framework_exe binary
 
-
 def run_command(command):
     try:
         # Run the command and capture the output
-        completed_process = subprocess.run(
-            command,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            shell=True,
-            text=True,
-        )
-
+        completed_process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+        
         # Print the stdout and stderr
         print(f"Command: {command}")
         # print("=== STDOUT ===")
         # print(completed_process.stdout)
         # print("=== STDERR ===")
         # print(completed_process.stderr)
-
+        
     except Exception as e:
         print(f"An error occurred while running the command: {e}")
-
 
 gt_path = "data/gaussian_example/gaussian_4_1000.gt"
 output_path = "./results/gaussian_4_1000.cluster"
 
-for density_method in [
-    "KthDistance",
-    "Normalized",
-    "ExpSquared",
-    "MutualKNN",
-    "RaceDensityComputer",
-]:
-    run_command(
-        "./build/dpc_framework_exe  --query_file ./data/gaussian_example/gaussian_4_1000.data \
+for density_method in ["KthDistance", "Normalized", "ExpSquared", "MutualKNN", "RaceDensityComputer"]:
+    run_command("./build/dpc_framework_exe  --query_file ./data/gaussian_example/gaussian_4_1000.data \
             --decision_graph_path ./results/gaussian_4_1000.dg --output_file %s \
-            --dist_cutoff 8.36 --density_method %s"
-        % (output_path, density_method)
-    )
+            --dist_cutoff 8.36 --density_method %s" % (output_path, density_method))
     metrics1 = cluster_eval.eval_clusters_wrapper(gt_path, output_path, verbose=True)
     print(metrics1)
