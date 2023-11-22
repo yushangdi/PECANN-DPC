22a456c36c7fc25ac39eb418d4439771922942df
Josh Engels <josh.adam.engels@gmail.com>
2023-11-21 19:43:56 -0500

    Fixing scalability test

--- experiments/paper/paper_utils.py
+++ experiments/paper/paper_utils.py
@@ -1,32 +1,11 @@
 import numpy as np
 import os
 from sklearn.utils import shuffle
-import sklearn
-
-
-def _closest_center(data, centers):
-    data = np.array(data)
-    centers = np.array(centers)
-
-    # Calculate the squared norms of data and centers
-    data_norms_squared = np.sum(data**2, axis=1, keepdims=True)
-    centers_norms_squared = np.sum(centers**2, axis=1, keepdims=True)
-
-    # Calculate the dot product between data and centers
-    dot_product = np.dot(data, centers.T)
-
-    # Calculate the squared distances without computing the square root
-    distances_squared = data_norms_squared - 2 * dot_product + centers_norms_squared.T
-
-    # Find the index of the closest center for each point
-    closest_centers = np.argmin(distances_squared, axis=1)
-
-    return closest_centers
 
 
 def generate_synthetic_data(num_datapoints, num_clusters, d=128, variance=0.05):
     points_per_cluster = num_datapoints // num_clusters
-    cluster_centers = np.random.uniform(low=10, high=11, size=(num_clusters, d))
+    cluster_centers = np.random.uniform(size=(num_clusters, d))
     data = []
     gts = []
 
@@ -43,9 +22,6 @@ def generate_synthetic_data(num_datapoints, num_clusters, d=128, variance=0.05):
     gt = np.array(gts, dtype=int)
     shuffle(data, gt, random_state=0)
 
-    closest_centers = _closest_center(data, cluster_centers)
-    # best_ari = sklearn.metrics.adjusted_rand_score(gt, closest_centers)
-    # print(points_per_cluster, "BEST", best_ari)
     return data, gt
 
 
