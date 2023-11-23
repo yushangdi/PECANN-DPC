67b37cf24ad31fae459cff05edeaed8f4b410df4
Josh Engels <josh.adam.engels@gmail.com>
2023-11-23 10:03:57 -0500

    Kmeans experiment

---
+++ experiments/paper/kmeans_pareto.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+import sys
+
+abspath = Path(__file__).resolve().parent.parent
+os.chdir(abspath)
+sys.path.append(str(abspath))
+
+from test_kmeans import run_kmeans
+
+for dataset, num_clusters in [
+    ("mnist", 10),
+    ("imagenet", 1000),
+    ("arxiv-clustering-s2s", 180),
+    ("reddit-clustering", 50),
+    ("birds", 525),
+]:
+    run_kmeans(dataset, prefix="kmeans_pareto")
